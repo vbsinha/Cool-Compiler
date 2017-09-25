13fc980746cb7eff56e5c328b601efe392edf137
Prateek Kumar <prateek@prateekkumar.in>
2017-09-25 23:24:07 +0530

    updated

--- semantic/src/java/cool/ClassTable.java
+++ semantic/src/java/cool/ClassTable.java
@@ -14,15 +14,8 @@ public class ClassTable{
 		objmethods.put("type_name", new AST.method("type_name", new ArrayList<AST.formal>(), "String", new AST.no_expr(0), 0));
 
 		classinfos.put("Object", new ClassInfo(null, new HashMap<String, AST.attr>(), objmethods, 0));
-		//height.put("Object", 0);
 
 		HashMap <String, AST.method> iomethods = new HashMap<String, AST.method>();
-
-		//List <AST.formal> out_string_formals = Arrays.asList(new AST.formal("out_string", "String", 0));
-		//List <AST.formal> os_formals = new ArrayList<AST.formal>();
-		//os_formals.add(new AST.formal("out_string", "String", 0));
-		//List <AST.formal> out_int_formals = Arrays.asList(new AST.formal("out_int", "Int", 0));
-		//oi_formals.add(new AST.formal("out_int", "Int", 0));
 
 		iomethods.put("out_string", new AST.method("out_string", Arrays.asList(new AST.formal("out_string", "String", 0))
 												   , "IO", new AST.no_expr(0), 0));
@@ -64,76 +57,70 @@ public class ClassTable{
 
 	void insert(AST.class_ c) {
 		String parent = c.parent;
-		ClassInfo currClass = new ClassInfo(c.parent, classinfos.get(c.parent).attrlist, classinfos.get(c.parent).methodlist, classinfos.get(c.parent).depth + 1);	// adding the parents attribute list and method list
+		ClassInfo currClass = new ClassInfo(c.parent, classinfos.get(c.parent).attrlist,
+			classinfos.get(c.parent).methodlist, classinfos.get(c.parent).depth + 1);	// adding the parents attribute list and method list
 
-
-		HashMap <String, AST.attr> currClassAttrList = new HashMap<String, AST.attr>();
-		HashMap <String, AST.method> currClassMethodList = new HashMap <String, AST.method>();
+		List<String> currClassAttrList = new ArrayList<>();
+		List<String> currClassMethodList = new ArrayList<>();
 
 		for(AST.feature feat : c.features) {
 			if(feat instanceof AST.attr) {
 				AST.attr attrfeat = (AST.attr) feat;
-				if(currClassAttrList.containsKey(attrfeat.name))
+				if(currClassAttrList.contains(attrfeat.name)) {
 					errors.add(new Error(c.filename, attrfeat.lineNo,
-							   "Attribute " + attrfeat.name + " is defined multiple times in class" + c.name));
-				else
-					currClassAttrList.put(attrfeat.name, attrfeat);
+							   "Attribute " + attrfeat.name + " is defined multiple times in class " + c.name));
+				}
+				else if (currClass.attrlist.containsKey(attrfeat.name)) {
+					errors.add(new Error(c.filename, attrfeat.lineNo, "Attribute " + attrfeat.name
+					 + " of class " + c.name + " is already an attribute of its parent class"));
+				} else {
+					currClass.attrlist.put(attrfeat.name, attrfeat);
+					currClassAttrList.add(attrfeat.name);
+				}
 			}
 			else if(feat instanceof AST.method) {
 				AST.method methodfeat = (AST.method) feat;
-				if(currClassMethodList.containsKey(methodfeat.name))
+				if(currClassMethodList.contains(methodfeat.name))
 					errors.add(new Error(c.filename, methodfeat.lineNo,
-							   "Method " + methodfeat.name + " is defined multiple times in class" + c.name));
-				else
-					currClassMethodList.put(methodfeat.name, methodfeat);
-			}
-		}
-
-		for(Entry<String, AST.attr> entry : currClassAttrList.entrySet()) {
-			if(currClass.attrlist.containsKey(entry.getKey()))
-				errors.add(new Error(c.filename, entry.getValue().lineNo, "Attribute " + entry.getValue().name + " of class " + c.name + " is already an attribute of its parent class"));
-			else {
-				currClass.attrlist.put(entry.getKey(), entry.getValue());
-			}
-
-		}
-
-		for(Entry<String, AST.method> entry : currClassMethodList.entrySet()) {
-			boolean foundErr = false;
-			if(currClass.methodlist.containsKey(entry.getKey())) {
-				AST.method parentMethod = currClass.methodlist.get(entry.getKey());
-				AST.method method = entry.getValue();
-				List<String> parameters = new ArrayList<String>();
-				for (AST.formal form : method.formals){
-					if(parameters.contains(form.name)){
-						errors.add(new Error(c.filename, method.lineNo, form.name +
-											" has been repeated more than once as parameter in " + method.name));
-						foundErr = true;
-					} else parameters.add(form.name);
-				}
-				if(method.formals.size() != parentMethod.formals.size()) {
-					errors.add(new Error(c.filename, method.lineNo, "Different number of parameters in redefined method "
-										+ method.name));
-					foundErr = true;
-				}
+							   "Method " + methodfeat.name + " is defined multiple times in class " + c.name));
 				else {
-					if(method.typeid.equals(parentMethod.typeid) == false) {
-						errors.add(new Error(c.filename, method.lineNo, "Return type" + method.typeid + " is differnt in redefined method " + method.name +  " from original return type " + parentMethod.typeid));
-						foundErr = true;
+					boolean foundErr = false;
+					List<String> parameters = new ArrayList<>();
+					for (AST.formal form : methodfeat.formals){
+						if(parameters.contains(form.name)){
+							errors.add(new Error(c.filename, methodfeat.lineNo, form.name +
+												" has been repeated more than once as parameter in " + methodfeat.name));
+							foundErr = true;
+						} else parameters.add(form.name);
 					}
-					for(int i = 0; i < method.formals.size(); ++i) {
-						if(method.formals.get(i).typeid.equals(parentMethod.formals.get(i).typeid) == false) {
-							errors.add(new Error(c.filename, method.lineNo, "Parameter type " + method.formals.get(i).typeid +" is differnt in redefined method " + method.name +  " from original return type " + parentMethod.formals.get(i).typeid));
+					if (currClass.methodlist.containsKey(methodfeat.name)) {
+						AST.method parentMethod = currClass.methodlist.get(methodfeat.name);
+
+						if(methodfeat.formals.size() != parentMethod.formals.size()) {
+							errors.add(new Error(c.filename, methodfeat.lineNo, "Different number of parameters in redefined method "
+												+ methodfeat.name));
 							foundErr = true;
 						}
+						else {
+							if(methodfeat.typeid.equals(parentMethod.typeid) == false) {
+								errors.add(new Error(c.filename, methodfeat.lineNo, "Return type" + methodfeat.typeid + " is differnt in redefined method " + methodfeat.name +  " from original return type " + parentMethod.typeid));
+								foundErr = true;
+							}
+							for(int i = 0; i < methodfeat.formals.size(); ++i) {
+								if(methodfeat.formals.get(i).typeid.equals(parentMethod.formals.get(i).typeid) == false) {
+									errors.add(new Error(c.filename, methodfeat.lineNo, "Parameter type " + methodfeat.formals.get(i).typeid +" is differnt in redefined method " + methodfeat.name +  " from original return type " + parentMethod.formals.get(i).typeid));
+									foundErr = true;
+								}
+							}
+						}
+					}
+					if (foundErr == false) {
+						currClass.methodlist.put(methodfeat.name, methodfeat);
+						currClassMethodList.add(methodfeat.name);
 					}
 				}
 			}
-
-			if(foundErr != true)
-				currClass.methodlist.put(entry.getKey(), entry.getValue());
 		}
-		//height.put(c.name, height.get(c.parent) + 1);
 
 		classinfos.put(c.name, currClass);
 	}
